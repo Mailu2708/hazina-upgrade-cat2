0c4e1917185928dd847becd2f028902b5e55a24d
Mailu2708 <laurynmailu@gmail.com>
2026-07-03 13:40:01 +0300

    Add localStorage theme and welcome features

--- submission/js/script.js
+++ submission/js/script.js
@@ -203,3 +203,59 @@ bookingForm.addEventListener("submit", function(event){
     bookingForm.reset();
 
 });
+
+
+// Theme Toggle
+
+const themeToggle = document.getElementById("themeToggle");
+
+// Check saved theme
+
+const savedTheme = localStorage.getItem("theme");
+
+if(savedTheme === "dark"){
+
+    document.body.classList.add("dark-mode");
+
+    themeToggle.textContent = "☀️ Light Mode";
+
+}
+
+themeToggle.addEventListener("click", function(){
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+
+        localStorage.setItem("theme","dark");
+
+        themeToggle.textContent = "☀️ Light Mode";
+
+    }
+
+    else{
+
+        localStorage.setItem("theme","light");
+
+        themeToggle.textContent = "🌙 Dark Mode";
+
+    }
+
+});
+
+// Welcome Returning Parent
+
+
+const welcomeHeading = document.getElementById("welcomeMessage");
+
+const storedParent = localStorage.getItem("parentName");
+
+if(storedParent){
+
+    welcomeHeading.innerHTML =
+
+    `Welcome back, ${storedParent}!<br>
+
+    Reliable & Safe School Transport for Your Peace of Mind`;
+
+}
